5cc5c22c7a5d77f264132955c505897461849ace
René Sebastián Pucheta <sebastian.pucheta@gmail.com>
2023-02-04 19:46:24 -0300

    Fixed rotation issues with ship.

--- utils.js
+++ utils.js
@@ -9,14 +9,18 @@ const colision = (a, b) => {
     a.y < b.y + b.h;
 }
 
-const radiants = (origin, target, degrees) => { 
+const radiants = (origin, target, degrees) => {
+  const toDegrees = (degrees) => degrees * (Math.PI / 180);
+
   if (target) {
-    let targetY = target.y < origin.y ? -(target.y) : target.y;
-    console.log(targetY);
-    return Math.atan2(origin.y + targetY, origin.x - target.x);
+    const distanceX = origin.x - target.x;
+    const distanceY = origin.y - target.y;
+    const angle = Math.atan2(distanceX, distanceY);
+
+    return angle;
   }
   if (degrees) {
-    return degrees * (Math.PI/180);
+    return toDegrees(degrees);
   }
 }
 
@@ -31,25 +35,25 @@ const pathLinear = function (origin, target, speed) {
   const limit = xIsLonger ? xSteps : ySteps;
   const xIsLongerSpeed = speed / (xSteps / ySteps);
   const yIsLongerSpeed = speed / (ySteps / xSteps);
-  for(let i = 0; i < limit; i++){
+  for (let i = 0; i < limit; i++) {
     if (xIsLonger) {
-      if(target.y < pos.y) {
+      if (target.y < pos.y) {
         pos.y = pos.y - xIsLongerSpeed;
       } else {
         pos.y = pos.y + xIsLongerSpeed;
       }
-      if(target.x < pos.x) {
+      if (target.x < pos.x) {
         pos.x = pos.x - speed;
       } else {
         pos.x = pos.x + speed;
       }
     } else {
-      if(target.y < pos.y) {
+      if (target.y < pos.y) {
         pos.y = pos.y - speed;
       } else {
         pos.y = pos.y + speed;
       }
-      if(target.x < pos.x) {
+      if (target.x < pos.x) {
         pos.x = pos.x - yIsLongerSpeed;
       } else {
         pos.x = pos.x + yIsLongerSpeed;
